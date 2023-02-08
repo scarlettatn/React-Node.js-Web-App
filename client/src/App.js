476fabdf9704d2e7af9d1833d5695745b4831fa5
Scarlett Ah-Thon <scarlettahthon@gmail.com>
2023-02-08 20:05:36 +0000

    addded skills css and jsx

--- client/src/App.js
+++ client/src/App.js
@@ -24,13 +24,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar></Navbar>
-        <About></About>
-        <Projects></Projects>
-        <Skills></Skills>
-        <Contact></Contact>
-        <h1>Here is a new feature</h1>
-        <h1>{this.state.response.body}</h1>
+        <div className="container-home">
+          <Navbar></Navbar>
+          <About></About>
+          <Projects></Projects>
+          <Skills></Skills>
+          <Contact></Contact>
+          <h1>Here is a new feature</h1>
+          <h1>{this.state.response.body}</h1>
+        </div>
       </div>
     );
   }
--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -13,12 +13,13 @@ function About() {
         and worked in various countries in multicultural
         teams (France, USA, UK, Luxembourg and Portugal).
       </p>
-        <div container>
+      <p>Read more</p>
+        <div className="container">
           <div className="button-contact">
             <a href="#contact">Work with me</a>
           </div>
           <div className="button-projects">
-            <a href="#projects">See my past projects</a>
+            <a href="#projects">See my projects</a>
           </div>
         </div>
     </div>
