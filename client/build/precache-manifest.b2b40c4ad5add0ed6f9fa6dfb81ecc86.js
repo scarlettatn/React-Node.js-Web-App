9fbde7ceafba4807a6d3ee01ccfbb2f7785d26dc
Scarlett Ah-Thon <scarlettahthon@gmail.com>
2024-08-08 23:08:28 +0100

    update dependencies

---
+++ client/build/precache-manifest.b2b40c4ad5add0ed6f9fa6dfb81ecc86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52aa035343c6f886401c0d9afc040f66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8123c0d61232c3aae1e1",
+    "url": "/static/css/main.78527746.chunk.css"
+  },
+  {
+    "revision": "d6177c4cf26e97788cb9",
+    "url": "/static/js/2.af41b654.chunk.js"
+  },
+  {
+    "revision": "4e6af70694a81408f015",
+    "url": "/static/js/3.41990134.chunk.js"
+  },
+  {
+    "revision": "8123c0d61232c3aae1e1",
+    "url": "/static/js/main.8862561f.chunk.js"
+  },
+  {
+    "revision": "60a1d56b33189d882521",
+    "url": "/static/js/runtime~main.9a640cb9.js"
+  }
+]);
